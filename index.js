619095031f447c4bf5e0e284d05fcde754582bf1
saman-zdf <samanfyz68@gmail.com>
2021-11-23 08:13:26 +0800

    express runnig

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
+// We can receive JSON data from POST/PUT/PATCH/etc request
+app.use(express.json());
+// The same as above but for forms
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (request, response) => {
+  response.json({ message: "It's express tutorials on tuesday" });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log('Server is running on: ' + HOST + PORT);
+});
